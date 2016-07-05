1040328ffcc8a21ba6209aa68a95ed0d443ba39e
0918901 <0918901@hr.nl>
2016-07-05 02:50:13 +0200

    File 3 - Stage 1 - Shoppinglist

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,35 @@
-$(document).ready(function () {
-    var addItem = $('#addItem');
-    var removeItem = $('#remove');
-    addItem.click(function () {
-        var toAdd = $('input#text').val();
-        $('#shoppingList').append('<li><input type="checkbox"/>' + toAdd + '</li>');
+function toevoegen() {
+    var lijst = document.getElementById("shoppingList");
+    for (var i = 0; i < 1; i++){
+        var text = $('input#text').val();
+        var node = document.createElement("li");
+        var input = document.createElement("input");
+        input.setAttribute("type", "checkbox");
+        input.setAttribute("id", i);
+        var textnode = document.createTextNode(text);
+        node.appendChild(input);
+        node.appendChild(textnode);
+        document.getElementById("shoppingList").appendChild(node);
+    }
+
+}
+function verwijderen() {
+    var verwijderen = document.getElementById('verwijderen');
+
+    verwijderen.addEventListener('click', function(){
+        var rmvCheckBoxes = document.getElementById('i');
+
+        for(var i = 0; i < 1; i++)
+        {
+            if(rmvCheckBoxes[i].checked)
+            {
+                removeElm(rmvCheckBoxes[i]);
+            }
+        }
     });
-    removeItem.click(function () {
-        var toRemove = $('input#text').val();
-        $('li:contains('+toRemove+')').remove();
-    });
-});
+
+    function removeElm(elm){
+        elm.parentElement.removeChild(elm);
+    }
+
+}
